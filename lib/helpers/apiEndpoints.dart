9a0e19dfe5857b0da22dd6b4ec282749062fc5b3
Vincent K. Kogei <42339869+mervinkogei@users.noreply.github.com>
2024-06-14 18:53:44 +0300

    refactor: Updated the api keys

--- lib/helpers/apiEndpoints.dart
+++ lib/helpers/apiEndpoints.dart
@@ -1,6 +1,6 @@
-class ApiEndointss {
-  static const rootURL =
-      'https://securus-standalone-api-nv53svhcdq-uc.a.run.app';
-  static const String baseURL = '$rootURL/api';
-  static const String aoiKey = '50094eca5f39b7cb646f0fa4b17e46f5';
+class ApiEndpoints {
+  static const rootURL = 'api.openweathermap.org';
+  static const String apiPath = "/data/3.0/";
+  static const String baseURL = '$rootURL/$apiPath';
+  static const String apiKey = '50094eca5f39b7cb646f0fa4b17e46f5';
 }
